2ebfad5d5dda00ec4a9cc314db6a4d3bcdfc13b6
Ruben Quintero <ruwenquintero@gmail.com>
2024-09-09 19:15:45 -0500

    se agrego otro comentario

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mongoose= require('mongoose');
 const dbconnect= ()=> {
 
     mongoose.set('strictQuery', true)
-    mongoose.connect ("mongodb://127.0.0.1:27017/login_adso")
+    mongoose.connect ("mongodb://127.0.0.1:27017/login_adso") //ruta de la base de datos de mongo//
     .then((success)=>console.log("conexion exitosa"))
     .catch( (err)=>console.log (err.mesagge));
 }
